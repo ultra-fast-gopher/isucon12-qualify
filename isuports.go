bb0d3b097534e10b3150a4a6f5ddd850b9814570
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2022-07-23 12:39:41 +0900

    Revert "sqlite3にcacheを追加"
    This reverts commit a654a22f710aa3acea686b43ecd9dab40fe7badf.

--- isuports.go
+++ isuports.go
@@ -80,7 +80,7 @@ func tenantDBPath(id int64) string {
 // テナントDBに接続する
 func connectToTenantDB(id int64) (*sqlx.DB, error) {
 	p := tenantDBPath(id)
-	db, err := sqlx.Open(sqliteDriverName, fmt.Sprintf("file:%s?mode=rw&_cache_size=10000000&cache=shared", p))
+	db, err := sqlx.Open(sqliteDriverName, fmt.Sprintf("file:%s?mode=rw", p))
 	if err != nil {
 		return nil, fmt.Errorf("failed to open tenant DB: %w", err)
 	}
